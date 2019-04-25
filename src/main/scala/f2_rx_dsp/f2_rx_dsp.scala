ef1722acfc53b5c407aaa9c180a6332fce25fad8
Marko Kosunen <marko.kosunen@aalto.fi>
2019-04-25 10:03:29 -0700

    Remove redundand signal class definitions

--- src/main/scala/f2_rx_dsp/f2_rx_dsp.scala
+++ src/main/scala/f2_rx_dsp/f2_rx_dsp.scala
@@ -67,19 +67,6 @@ import dcpipe._
 import clkmux._
 
 
-//Consider using "unit" instead of "User" 
-class usersigs (val n: Int, val users: Int=4) extends Bundle {
-    val udata=DspComplex(SInt(n.W), SInt(n.W))
-    val uindex=UInt(log2Ceil(users).W)
-}
-
-class iofifosigs(val n: Int, val users: Int=4 ) extends Bundle {
-        //4=Users
-        val data=Vec(users,new usersigs(n=n,users=users))
-        val rxindex=UInt(2.W)
-        override def cloneType = (new iofifosigs(n,users)).asInstanceOf[this.type]
-}
-
 class f2_rx_dsp_io(
         val inputn    : Int=9, 
         val n         : Int=16, 
